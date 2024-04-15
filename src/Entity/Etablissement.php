5f82c8c9f23e41e9feed3bd888779e59ec871b37
mannai marwa <marwa.mannai@esprit.tn>
2024-04-15 23:40:00 +0100

    update

--- src/Entity/Etablissement.php
+++ src/Entity/Etablissement.php
@@ -2,37 +2,145 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
+use DateTime;
 use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\EtablissementRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Table(name: "etablissement")]
-#[ORM\Entity]
+
+#[ORM\Entity(repositoryClass: EtablissementRepository::class)]
+
 class Etablissement
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: "IDENTITY")]
-    #[ORM\Column(name: "ID_Etablissement", type: "integer", nullable: false)]
-    private $idEtablissement;
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    #[ORM\Column(name: "ID_Etablissement", type: "integer")]
+    private ?int $ID_Etablissement;
 
-    #[ORM\Column(name: "img_Etablissement", type: "string", length: 255, nullable: false)]
-    private $imgEtablissement;
 
-    #[ORM\Column(name: "Nom_Etablissement", type: "string", length: 255, nullable: false)]
-    private $nomEtablissement;
+    #[ORM\Column(length :255)]
+    private ?string $imgEtablissement ;
 
-    #[ORM\Column(name: "Adresse_Etablissement", type: "string", length: 255, nullable: false)]
-    private $adresseEtablissement;
+    #[ORM\Column(length :255)]
+    #[Assert\NotBlank(message: 'The establishment name cannot be blank.')]
+    #[Assert\Type(type: 'string', message: 'The establishment name must be a string.')]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z ]+$/',
+        message: 'The establishment name can only contain letters and spaces.'
+    )]
+    private ?string $nomEtablissement;
 
-    #[ORM\Column(name: "Type_Etablissement", type: "string", length: 255, nullable: false)]
-    private $typeEtablissement;
+    #[ORM\Column(length :255)]
+    private ?string $adresseEtablissement;
 
-    #[ORM\Column(name: "Tel_Etablissement", type: "integer", nullable: false)]
-    private $telEtablissement;
+    #[ORM\Column(length :255)]
+    private ?string $typeEtablissement;
 
-    #[ORM\Column(name: "Directeur_Etablissement", type: "string", length: 255, nullable: false)]
-    private $directeurEtablissement;
+    #[ORM\Column(type: "integer")]
+    private ?int $telEtablissement;
 
-    #[ORM\Column(name: "Date_Fondation", type: "date", nullable: false)]
-    private $dateFondation;
+    #[ORM\Column(length :255)]
+    #[Assert\NotBlank(message: 'The director name cannot be blank.')]
+    #[Assert\Type(type: 'string', message: 'The director name must be a string.')]
+    #[Assert\Regex(
+        pattern: '/^[a-zA-Z ]+$/',
+        message: 'The director name can only contain letters and spaces.'
+    )]
+    private ?string$directeurEtablissement;
+
+    #[ORM\Column(type: "datetime")]
+    #[Assert\LessThan("today", message: "The date of foundation must be less than today")]
+    private ?DateTime $dateFondation;
+
+    public function getIdEtablissement(): ?int
+    {
+        return $this->ID_Etablissement;
+    }
+
+    public function getImgEtablissement(): ?string
+    {
+        return $this->imgEtablissement;
+    }
+
+    public function setImgEtablissement(string $imgEtablissement): static
+    {
+        $this->imgEtablissement = $imgEtablissement;
+
+        return $this;
+    }
+
+    public function getNomEtablissement(): ?string
+    {
+        return $this->nomEtablissement;
+    }
+
+    public function setNomEtablissement(string $nomEtablissement): static
+    {
+        $this->nomEtablissement = $nomEtablissement;
+
+        return $this;
+    }
+
+    public function getAdresseEtablissement(): ?string
+    {
+        return $this->adresseEtablissement;
+    }
+
+    public function setAdresseEtablissement(string $adresseEtablissement): static
+    {
+        $this->adresseEtablissement = $adresseEtablissement;
+
+        return $this;
+    }
+
+    public function getTypeEtablissement(): ?string
+    {
+        return $this->typeEtablissement;
+    }
+
+    public function setTypeEtablissement(string $typeEtablissement): static
+    {
+        $this->typeEtablissement = $typeEtablissement;
+
+        return $this;
+    }
+
+    public function getTelEtablissement(): ?int
+    {
+        return $this->telEtablissement;
+    }
+
+    public function setTelEtablissement(int $telEtablissement): static
+    {
+        $this->telEtablissement = $telEtablissement;
+
+        return $this;
+    }
+
+    public function getDirecteurEtablissement(): ?string
+    {
+        return $this->directeurEtablissement;
+    }
+
+    public function setDirecteurEtablissement(string $directeurEtablissement): static
+    {
+        $this->directeurEtablissement = $directeurEtablissement;
+
+        return $this;
+    }
+
+    public function getDateFondation(): ?\DateTimeInterface
+    {
+        return $this->dateFondation;
+    }
+
+    public function setDateFondation(\DateTimeInterface $dateFondation): static
+    {
+        $this->dateFondation = $dateFondation;
+
+        return $this;
+    }
+
+
 }
-
---
+++ src/Form/EtablissementType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etablissement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class EtablissementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imgEtablissement',FileType::class, [
+            'label'=>false,
+            'mapped'=>false,
+            'required'=>false
+            ])
+            ->add('nomEtablissement', null, [
+                'label' => 'Nom de l\'établissement', // Label in French
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le nom de l\'établissement ne doit pas être vide.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/',
+                        'message' => 'Le nom de l\'établissement ne doit contenir que des lettres et des espaces.',
+                    ]),
+                ],
+            ])
+            ->add('adresseEtablissement')
+            ->add('typeEtablissement', ChoiceType::class, [
+                'label' => 'Type',
+                'choices' => [
+                    'École' => 'École',
+                    'Université' => 'Université',
+                    'Centre de formation' => 'Centre de formation',
+                    'Université virtuelle' => 'Université virtuelle',
+                    'Institut Supérieur' => 'Institut Supérieur',
+                    'Ecole ing' => 'Ecole Nationale d\'Ingénieurs',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('telEtablissement', null, [
+                'label' => 'Numéro de téléphone de l\'établissement', // Label in French
+                'constraints' => [
+                    new Assert\Length([
+                        'min' => 8,
+                        'max' => 8,
+                        'exactMessage' => 'The phone number must contain exactly {{ limit }} digits.',
+                    ]),
+                ],
+            ])
+            ->add('directeurEtablissement')
+            ->add('dateFondation', DateType::class, [
+                'widget' => 'single_text',
+                'required' => false,
+                'placeholder' => [
+                    'year' => 'YYYY',
+                    'month' => 'MM',
+                    'day' => 'DD',
+                ],
+                'data' => new \DateTime(),
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Etablissement::class,
+        ]);
+    }
+}
